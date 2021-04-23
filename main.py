6de1844a6550712f0f491732b52b62e60544ee1d
Plextora <>
2021-04-23 22:37:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+name = input("What's your name?: ")
+favorite_color = input("What's your favorite color: ")
+print(name + " Likes " + favorite_color + "! :D That's a great color. Lets play some time again!")  
